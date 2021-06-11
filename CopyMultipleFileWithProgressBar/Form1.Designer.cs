7a36f8929181954bf12fb4f016b8d89736d558ff
unknown <tjtik83@gmail.com>
2021-06-11 10:31:26 +0700

    Completed Project

---
+++ CopyMultipleFileWithProgressBar/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace CopyMultipleFileWithProgressBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.btnChooseFile = new System.Windows.Forms.Button();
+            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.btnDestination = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Worker = new System.ComponentModel.BackgroundWorker();
+            this.lblPercentage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(334, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File(s)";
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 18;
+            this.listBox.Location = new System.Drawing.Point(18, 31);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(601, 130);
+            this.listBox.TabIndex = 1;
+            // 
+            // btnChooseFile
+            // 
+            this.btnChooseFile.Location = new System.Drawing.Point(625, 132);
+            this.btnChooseFile.Name = "btnChooseFile";
+            this.btnChooseFile.Size = new System.Drawing.Size(36, 29);
+            this.btnChooseFile.TabIndex = 2;
+            this.btnChooseFile.Text = "....";
+            this.btnChooseFile.UseVisualStyleBackColor = true;
+            this.btnChooseFile.Click += new System.EventHandler(this.btnChooseFile_Click);
+            // 
+            // txtDestination
+            // 
+            this.txtDestination.Location = new System.Drawing.Point(18, 198);
+            this.txtDestination.Name = "txtDestination";
+            this.txtDestination.Size = new System.Drawing.Size(601, 24);
+            this.txtDestination.TabIndex = 3;
+            // 
+            // btnDestination
+            // 
+            this.btnDestination.Location = new System.Drawing.Point(625, 196);
+            this.btnDestination.Name = "btnDestination";
+            this.btnDestination.Size = new System.Drawing.Size(36, 29);
+            this.btnDestination.TabIndex = 4;
+            this.btnDestination.Text = "....";
+            this.btnDestination.UseVisualStyleBackColor = true;
+            this.btnDestination.Click += new System.EventHandler(this.btnDestination_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(18, 260);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(643, 30);
+            this.progressBar.TabIndex = 5;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(300, 298);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(83, 29);
+            this.btnCopy.TabIndex = 6;
+            this.btnCopy.Text = "&Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 177);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 18);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Destination Folder : ";
+            // 
+            // Worker
+            // 
+            this.Worker.WorkerReportsProgress = true;
+            // 
+            // lblPercentage
+            // 
+            this.lblPercentage.AutoSize = true;
+            this.lblPercentage.Location = new System.Drawing.Point(324, 239);
+            this.lblPercentage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPercentage.Name = "lblPercentage";
+            this.lblPercentage.Size = new System.Drawing.Size(33, 18);
+            this.lblPercentage.TabIndex = 8;
+            this.lblPercentage.Text = "0 %";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 339);
+            this.Controls.Add(this.lblPercentage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnDestination);
+            this.Controls.Add(this.txtDestination);
+            this.Controls.Add(this.btnChooseFile);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Copy Multiple File With ProgressBar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button btnChooseFile;
+        private System.Windows.Forms.TextBox txtDestination;
+        private System.Windows.Forms.Button btnDestination;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Label label2;
+        private System.ComponentModel.BackgroundWorker Worker;
+        private System.Windows.Forms.Label lblPercentage;
+    }
+}
+
---
+++ CopyMultipleFileWithProgressBar/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CopyMultipleFileWithProgressBar
+{
+    public partial class Form1 : Form
+    {
+
+        BackgroundWorker worker = new BackgroundWorker();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = false;
+
+            worker.DoWork += Worker_DoWork;
+            worker.ProgressChanged += Worker_ProgressChanged;
+        }
+
+        private void CopyFIle(string source, string destination)
+        {
+            FileStream fsOut = new FileStream(destination, FileMode.Create);
+            FileStream fsIn = new FileStream(source, FileMode.Open);
+            byte[] bt = new byte[1048756];
+            int readByte;
+            while ((readByte = fsIn.Read(bt, 0, bt.Length)) > 0)
+            {
+                fsOut.Write(bt, 0, readByte);
+                worker.ReportProgress((int)(fsIn.Position * 100 / fsIn.Length));
+            }
+            fsIn.Close();
+            fsOut.Close();
+        }
+
+
+        private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+            lblPercentage.Text = progressBar.Value.ToString() + " %";
+            if (progressBar.Value == 100)
+            {
+                progressBar.Value = 0;
+            }
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < listBox.Items.Count; i++)
+            {
+                CopyFIle(listBox.Items[i].ToString(), txtDestination.Text+"\\"+Path.GetFileName(listBox.Items[i].ToString()));
+            }
+            MessageBox.Show("OK");
+        }
+
+        private void btnChooseFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opd = new OpenFileDialog();
+            opd.Multiselect = true;
+            if(opd.ShowDialog() == DialogResult.OK)
+            {
+                listBox.Items.AddRange(opd.FileNames);
+            }
+        }
+
+        private void btnDestination_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                txtDestination.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            worker.RunWorkerAsync();
+        }
+    }
+}
